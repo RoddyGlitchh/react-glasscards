c1d9d9f03fb5443e80e0aa184521ad50a9dd07d5
RoddyGlitchh <mohsin.kamal.akbar@gmail.com>
2022-07-04 13:16:33 +0200

    removeed example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import React from 'react'
-import styles from './styles.module.css'
+import './GlassCard_styles.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import React from 'react'
+const GlassCard = ({ color, tilt, width, height, children }) => {
+  let cardwidth
+  let cardheight
+  let tiltable
+  if (tilt === true) {
+    tiltable = 'js-tilt'
+  } else {
+    tiltable = ''
+  }
+  if (width === null) {
+    cardwidth = 10
+  } else {
+    cardwidth = width
+  }
+  if (height === null) {
+    cardheight = 15
+  } else {
+    cardheight = height
+  }
+
+  return (
+    <div
+      className={`glass-container ${tiltable} light`}
+      style={{
+        backgroundColor: color,
+        width: `${cardwidth}rem`,
+        height: `${cardheight}rem`
+      }}
+    >
+      {children}
+    </div>
+  )
 }
+
+export default GlassCard
