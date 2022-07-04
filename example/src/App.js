f628b31028f8f4f84b64ae7b7c18018defe8bbfa
RoddyGlitchh <mohsin.kamal.akbar@gmail.com>
2022-07-04 12:47:03 +0200

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import './global/styles.css'
 
+import React, { useEffect } from 'react'
+import VanillaTilt from 'vanilla-tilt'
 import { ExampleComponent } from 'react-glasscards'
-import 'react-glasscards/dist/index.css'
+import GlassCard from './GlassCard/GlassCard'
+// import 'react-glasscards/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  useEffect(() => {
+    VanillaTilt.init(document.querySelectorAll('.js-tilt'), {
+      max: 7,
+      glare: true
+    })
+  }, [])
+
+  return (
+    <GlassCard
+      color={'rgba(116, 125, 140, 0.4)'}
+      tilt={true}
+      width={10}
+      height={15}
+    >
+      <p>This is heading</p>
+      <h1>This is heading</h1>
+    </GlassCard>
+  )
 }
 
 export default App
