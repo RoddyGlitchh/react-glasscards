b14d10d08661aea44bef6a3c25c6d0ea793b6174
RoddyGlitchh <mohsin.kamal.akbar@gmail.com>
2022-07-04 14:19:31 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css'
 import React from 'react'
-const GlassCard = ({ color, width, height, children }) => {
+const GlassCard = ({ color, width, height, children, glassOpacity = 0.5 }) => {
   let cardwidth
   let cardheight
 
@@ -14,28 +14,24 @@ const GlassCard = ({ color, width, height, children }) => {
   } else {
     cardheight = height
   }
-
-  const glass_style = {
-    padding: '10px',
-    borderRadius: '8px',
-    backdropFilter: 'blur(10px)',
-    border: 'solid 1px rgba(255, 255, 255, 0.18)',
-    boxShadow: 'inset',
-    boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.1)',
-    boxSizing: 'border-box',
-    maxWidth: '25rem',
-    maxHeight: '40rem',
-    overflow: 'hidden',
-    position: 'relative',
-    margin: '4rem'
+  function hexToRgbA(hex) {
+    var c
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+      c = hex.substring(1).split('')
+      if (c.length == 3) {
+        c = [c[0], c[0], c[1], c[1], c[2], c[2]]
+      }
+      c = '0x' + c.join('')
+      return [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',')
+    }
+    throw new Error('Bad Hex')
   }
 
   return (
     <div
       className={`glass-container`}
       style={{
-        glass_style,
-        backgroundColor: color,
+        backgroundColor: `rgba(${hexToRgbA(color)},${glassOpacity})`,
         width: `${cardwidth}rem`,
         height: `${cardheight}rem`
       }}
