0a1ec49274e96cf63794cd0943c7124381621275
RoddyGlitchh <mohsin.kamal.akbar@gmail.com>
2022-07-04 14:12:19 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -15,10 +15,26 @@ const GlassCard = ({ color, width, height, children }) => {
     cardheight = height
   }
 
+  const glass_style = {
+    padding: '10px',
+    borderRadius: '8px',
+    backdropFilter: 'blur(10px)',
+    border: 'solid 1px rgba(255, 255, 255, 0.18)',
+    boxShadow: 'inset',
+    boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.1)',
+    boxSizing: 'border-box',
+    maxWidth: '25rem',
+    maxHeight: '40rem',
+    overflow: 'hidden',
+    position: 'relative',
+    margin: '4rem'
+  }
+
   return (
     <div
       className={`glass-container`}
       style={{
+        glass_style,
         backgroundColor: color,
         width: `${cardwidth}rem`,
         height: `${cardheight}rem`
